1e2f0fe6ca7551ef5ab7c7b782f6c99020d8a93b
Tatrakrad <riddick.taylor@gmail.com>
2017-01-26 16:41:14 -0600

    branch for map testing

--- client/js/components/MapComponent.js
+++ client/js/components/MapComponent.js
@@ -4,9 +4,24 @@ if (window.AL === undefined){window.AL = {}; }
 
   class MapComponent extends React.Component{
 
+
+    componentDidMount() {
+      console.log('this map', this.map);
+      this.googleMap = new google.maps.Map(this.map, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 10
+      });
+    }
+
     render(){
 
-      return (<div>Map Component</div>);
+      return (<div>Map Component
+        <div>
+          <div ref={(map) =>
+            { this.map = map; }} style={{width: '100%', height: '400px'}}>
+          </div>
+        </div>
+      </div>);
     }
   }
 
--- public/all.js
+++ public/all.js
@@ -515,7 +515,7 @@ if (window.AL === undefined) {
       });
     } };
 })();
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -540,13 +540,30 @@ if (window.AL === undefined) {
     }
 
     _createClass(MapComponent, [{
-      key: "render",
+      key: 'componentDidMount',
+      value: function componentDidMount() {
+        console.log('this map', this.map);
+        this.googleMap = new google.maps.Map(this.map, {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 10
+        });
+      }
+    }, {
+      key: 'render',
       value: function render() {
+        var _this2 = this;
 
         return React.createElement(
-          "div",
+          'div',
           null,
-          "Map Component"
+          'Map Component',
+          React.createElement(
+            'div',
+            null,
+            React.createElement('div', { ref: function ref(map) {
+                _this2.map = map;
+              }, style: { width: '100%', height: '400px' } })
+          )
         );
       }
     }]);
