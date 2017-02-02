223e2f3c57424c98f7f9c74e6069c91c8151f5f8
Tatrakrad <riddick.taylor@gmail.com>
2017-02-02 11:46:20 -0600

    ejs working, only with path to map

--- server/server.js
+++ server/server.js
@@ -27,8 +27,10 @@ app.use(require('./api-routes.js')());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', (req, res) => { res.render('index.ejs'); });
-app.get('/#', (req, res) => { res.render('app.ejs'); });
+app.get('/', (req, res) => { res.render('app.ejs'); });
+// app.get('/'),(req, res) => {res.render('index.ejs');});
+// app.get('/map', (req, res) => { res.render('app.ejs'); });
+
 
 var port = process.env.PORT || 5056;
 
